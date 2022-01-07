58814795f25fb0c78a9a8407bcc2c237ff0ee425
Vanillainanutshell <maurovasconcelos11@gmail.com>
2022-01-07 17:37:31 -0200

    Add changes on stylesabout and styles

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
 });
+const closebutton = document.getElementById('close')
+const mobilemenu = document.getElementById('mobile-demo')
+
+closebutton.onclick = function closemenu() {
+    mobilemenu.style.display = 'none';
+}
